9274e104faea86e1761957fa4281238d3cc83fee
michelriondel <56193034+michelriondel@users.noreply.github.com>
2020-01-22 22:46:25 +0100

    Add files via upload

---
+++ ldapmvc/src/test/java/com/riondel/ldapmvc/LdapmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riondel.ldapmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
